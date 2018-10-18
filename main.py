df7baf0a0809d78adadc0953ef56a395de0b368c
aedades <amanda.edades@gmail.com>
2018-10-17 21:43:15 -0700

    Refactor redraw_game_window()

--- main.py
+++ main.py
@@ -52,27 +52,12 @@ user = User(83, 560, 10, 10, 'res/ghost-trick-or-treating-hi.png')
 
 
 def redraw_game_window():
-    # draw background
-    win.blit(bg, (0, 0))
-    # draw current level
-    level_text = (font.render(f'level: {level}', 1, (0, 0, 0)))
-    win.blit(level_text, (1150, 10))
-    # draw health with background
-    health = font.render(f'lives: {lives} ', 1, (255, 255, 255))  # 81x21
-    pygame.draw.rect(win, (160, 160, 160), (589.5, 90, 105, 41))
-    win.blit(health, (599.5, 100))
-    # draw question
-    question_text = font.render(question, 1, (255, 255, 255))
-    pygame.draw.rect(win, (160, 160, 160), (547, 190, 186, 41))
-    win.blit(question_text, (557, 200))  # 166, 21
-    # draw sprite
-    win.blit(user.image, (user.x, user.y, user.width, user.height))
-    # update
-    answer_txt = font.render(text, True, (255, 255, 255))
-    width = max(200, answer_txt.get_width() + 10)
-    input_box.w = width
-    win.blit(answer_txt, (input_box.x + 5, input_box.y + 5))
-    pygame.draw.rect(win, (255, 100, 0), input_box, 2)
+    draw_background()
+    draw_current_level()
+    draw_health()
+    draw_question()
+    draw_sprite()
+    update_input_box()
 
     if is_dead:
         win.blit(u_dead, (0, 0, 1280, 720))
@@ -81,7 +66,41 @@ def redraw_game_window():
         # TODO
         # make screen or animation for winning
         new_game_button.draw(win)
+
     pygame.display.update()
+
+
+def draw_background():
+    win.blit(bg, (0, 0))
+
+
+def draw_current_level():
+    level_text = (font.render(f'level: {level}', 1, (0, 0, 0)))
+    win.blit(level_text, (1150, 10))
+
+
+def draw_health():
+    health = font.render(f'lives: {lives} ', 1, (255, 255, 255))  # 81x21
+    pygame.draw.rect(win, (160, 160, 160), (589.5, 90, 105, 41))
+    win.blit(health, (599.5, 100))
+
+
+def draw_question():
+    question_text = font.render(question, 1, (255, 255, 255))
+    pygame.draw.rect(win, (160, 160, 160), (547, 190, 186, 41))
+    win.blit(question_text, (557, 200))  # 166, 21
+
+
+def draw_sprite():
+    win.blit(user.image, (user.x, user.y, user.width, user.height))
+
+
+def update_input_box():
+    answer_text = font.render(text, True, (255, 255, 255))
+    answer_width = max(200, answer_text.get_width() + 10)
+    input_box.w = answer_width
+    win.blit(answer_text, (input_box.x + 5, input_box.y + 5))
+    pygame.draw.rect(win, (255, 100, 0), input_box, 2)
 
 
 def get_question():
@@ -131,13 +150,17 @@ while 1:
                 for i in range(20):
                     user.x += 10.25
                     redraw_game_window()
+
                 while progress >= 5:
                     # make screen freeze so you cant input more
                     redraw_game_window()
+
                     for event in pygame.event.get():
                         pos = pygame.mouse.get_pos()
+
                         if event.type == pygame.QUIT:
                             pygame.quit()
+
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             if new_game_button.is_over(pos):
                                 progress = 0
@@ -151,7 +174,9 @@ while 1:
                 if lives == 0:
                     is_dead = True
                     get_new_question = True
+
         redraw_game_window()
+
     while is_dead:
         for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
